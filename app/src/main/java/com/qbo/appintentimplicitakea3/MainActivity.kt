6f2960875c3f89483389463d1e07ee9a2b582053
LuisAngel <luisangel.salvat@gmail.com>
2021-02-11 18:44:23 -0500

    primera parte (110221)

--- app/src/main/java/com/qbo/appintentimplicitakea3/MainActivity.kt
+++ app/src/main/java/com/qbo/appintentimplicitakea3/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.qbo.appintentimplicitakea3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.qbo.appintentimplicitakea3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val vista = binding.root
+        setContentView(vista)
+
+        binding.btncompartir.setOnClickListener {
+            val enviarIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Hola mundo")
+                type = "text/plain"
+            }
+            if(enviarIntent.resolveActivity(packageManager) != null){
+                startActivity(enviarIntent)
+            }
+        }
+
     }
 }
